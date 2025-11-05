ad5e339c96f1390e006b7e8465c50acac2a2f377
Lakshman <laxmannani960@gmail.com>
2025-11-05 15:29:14 +0530

    largest elemnet

--- array/LargestElement.java
+++ array/LargestElement.java
@@ -1,7 +1,20 @@
-package array;
+
 
 public class LargestElement{
-    public static void main(String args[]){
-        System.out.println("iam working");
+    public static int findLargest(int[] arr) {
+        int largest = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            } else {
+                i++;
+            }
+        }
+            return largest;
+    }
+        public static void main(String args[]){
+        int arr[] = {1,2,3,5,25,8,12};
+        int largestNum = findLargest(arr);
+        System.out.println(largestNum);
     }
 }
