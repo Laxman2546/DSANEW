80ed3543d3dbef94ef8ef5aa63bb8c206b1deaf8
Lakshman <laxmannani960@gmail.com>
2026-02-10 11:53:15 +0530

    harshad number

--- src/Isugly.java
+++ src/Isugly.java
@@ -1,17 +1,27 @@
 public class Isugly {
     public static void main(String[] args) {
-        int n = 10;
-            if(n<=0){
-                System.out.println(false);
-            }
-            int[] factors = {2,3,5};
-            for(int fact : factors){
-                while(n%fact == 0 ){
-                    n/=fact;
-                }
-            }
-             System.out.println(n==1);
+        int x = 23;
+//            if(n<=0){
+//                System.out.println(false);
+//            }
+//            int[] factors = {2,3,5};
+//            for(int fact : factors){
+//                while(n%fact == 0 ){
+//                    n/=fact;
+//                }
+//            }
+//             System.out.println(n==1);
+//        }
+        int sum = 0;
+        int actual = x;
+        while (x != 0) {
+            int temp = x % 10;
+            sum += temp;
+            x = x / 10;
         }
-
-
+        if (actual % sum == 0) {
+            System.out.println(sum);
+        }
+        System.out.println(-1);
+    }
 }
