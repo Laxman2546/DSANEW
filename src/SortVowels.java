d073aad4078f11aee6ba1b51d66a3207ba009845
Lakshman <laxmannani960@gmail.com>
2026-04-27 13:50:14 +0530

    hashmap changes

--- src/SortVowels.java
+++ src/SortVowels.java
@@ -10,8 +10,7 @@ public class SortVowels {
         for(int i =0;i<s.length();i++){
             char c = s.charAt(i);
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                // Use the character 'c' as the key, not the whole string 's'
-                hm.put(c, hm.getOrDefault(c, 0) + 1);
+
             }
         }
     }
