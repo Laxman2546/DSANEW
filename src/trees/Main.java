8699ab1b39cd497158bac240db0bc3dd56b5d752
Lakshman <laxmannani960@gmail.com>
2026-05-08 12:54:39 +0530

    orders and trees

--- src/trees/Main.java
+++ src/trees/Main.java
@@ -74,11 +74,99 @@ class Binary{
         display(node.right, " ");
     }
 }
+class Tree{
+    class Node{
+        int value;
+        Node left;
+        Node right;
+        public  Node(int value){
+            this.value = value;
+        }
+    }
+    private Node root;
+    public void addNode(int value){
+        root = addEle(root,value);
+    }
 
+    private Node addEle(Node node, int value) {
+        if(node == null){
+           node = new Node(value);
+           return node;
+        }
+        if(value < node.value){
+            node.left = addEle(node.left,value);
+        }else{
+            node.right = addEle(node.right,value);
+        }
+        return  node;
+    }
+    public void populate(int[] nums){
+        populate(nums,0,nums.length);
+    }
+    public void populate(int[] nums,int st,int end){
+        if(st >= end){
+            return;
+        }
+        int mid = st+ (end - st) /2;
+        addNode(nums[mid]);
+        populate(nums,st,mid);
+        populate(nums,mid+1,end);
+    }
+    public void display(){
+        prettyDispaly(this.root,"this is root node");
+    }
+    public void preOrder(){
+        preOrder(root);
+    }
+    public  void  preOrder(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.println(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    public void postOrder(){
+        postOrder(root);
+    }
+    public  void  postOrder(Node node){
+        if(node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value);
+    }
+    public void inOrder(){
+        inOrder(root);
+    }
+    public  void  inOrder(Node node){
+        if(node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.value);
+        inOrder(node.right);
+    }
+    private void prettyDispaly(Node node,String details) {
+        if(node == null){
+            return;
+        }
+        System.out.println(details+" "+node.value);
+        prettyDispaly(node.left,"left node of value"+node.value);
+        prettyDispaly(node.right,"right node of value" + node.value);
+
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        Binary bin = new Binary();
-        bin.createNode();
-        bin.display();
+//        Binary bin = new Binary();
+//        bin.createNode();
+//        bin.display();
+        Tree tr = new Tree();
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        tr.populate(nums);
+        tr.inOrder();
+        tr.display();
     }
 }
