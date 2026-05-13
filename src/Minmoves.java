9ee387d6c71b89cd61f344dc43e3a3babd214582
Lakshman <laxmannani960@gmail.com>
2026-05-13 16:50:46 +0530

    learn again

---
+++ src/Minmoves.java
@@ -0,0 +1,35 @@
+public class Minmoves {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,1};
+        System.out.println(minMoves(nums,2));
+    }
+//54,50,66,107
+
+
+    public static  int minMoves(int[] nums, int limit) {
+        int n = nums.length;
+        int[] diff = new int[2 * limit + 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            int a = nums[i], b = nums[n - 1 - i];
+            int low = Math.min(a, b) + 1;
+            int high = Math.max(a, b) + limit;
+
+            diff[2] += 2;
+            diff[low] -= 1;
+            diff[a + b] -= 1;
+            diff[a + b + 1] += 1;
+            diff[high + 1] += 1;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        int curr = 0;
+
+        for (int sum = 2; sum <= 2 * limit; sum++) {
+            curr += diff[sum];
+            ans = Math.min(ans, curr);
+        }
+
+        return ans;
+    }
+}
