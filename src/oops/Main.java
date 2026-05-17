4b44603d756bc292cfe342b6e8f70bb5732a6296
Lakshman <laxmannani960@gmail.com>
2026-05-17 20:12:32 +0530

    practice more

--- src/oops/Main.java
+++ src/oops/Main.java
@@ -4,7 +4,7 @@ import oops.StudentDet;
 public class Main{
     public static void main(String[] args) {
         StudentDet sd = new StudentDet();
-        sd.name("Lakshman");
+        sd.name("Nani");
         sd.age(21);
     }
 }
