77395883d917c35b06d6d748f7761a73f012703a
Lakshman <laxmannani960@gmail.com>
2026-05-20 18:50:14 +0530

    prefix array

---
+++ src/PrefixArr.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class PrefixArr {
+    public static void main(String[] args) {
+        int[] A = {1,3,2,4};
+        int[] B = {3,1,2,4};
+        int[] res = findThePrefixCommonArray(A,B);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int l = 0;
+        int r = 0;
+        int count = 0;
+        int k = 0;
+        int[] arr = new int[A.length];
+        HashSet<Integer> hs = new HashSet<>();
+        while(l < A.length && r < B.length){
+            if(A[l] == B[r]){
+                hs.add(A[0]);
+                hs.add(B[0]);
+                count++;
+            }
+
+            if(hs.contains(A[l])){
+                count++;
+            }
+            if(hs.contains(B[r])){
+                count++;
+            }
+            if(!hs.contains(A[l]) || !hs.contains(B[r])){
+                hs.add(A[l]);
+                hs.add(B[r]);
+            }
+            arr[k++] = arr[k-1] + count;
+            l++;
+            r++;
+        }
+        return arr;
+    }
+}
