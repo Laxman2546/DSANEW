8dc831922b06d02cee77fce4798f82405c5c9990
Lakshman <laxmannani960@gmail.com>
2026-07-16 08:39:32 +0530

    GCD sum

---
+++ src/SumofGcd.java
@@ -0,0 +1,9 @@
+public class SumofGcd {
+    public static void main(String[] args) {
+        int[] nums = {2,6,4};
+        gcdSum(nums);
+    }
+    public static long gcdSum(int[] nums){
+        return 0;
+    }
+}
