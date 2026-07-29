1a27095b7d42ffe83dda8b3fc065afdc1e27c8a5
Lakshman <laxmannani960@gmail.com>
2026-07-29 08:40:57 +0530

    coin problem

--- src/StringTrim.java
+++ src/StringTrim.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.PriorityQueue;
 class Pairs{
     int value;
@@ -40,5 +41,29 @@ public class StringTrim {
             System.out.println(Arrays.toString(digits));
         }
         return res;
+    }
+}
+
+
+//coin problem
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String coins = sc.nextLine();
+        System.out.println(maxGold(coins));
+    }
+    public static int maxGold(String coins){
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i=0;i<coins.length();i++){
+            char coin = coins.charAt(i);
+            hm.put(coin,hm.getOrDefault(coin,0)+1);
+        }
+        int bronze = hm.containsKey('B') ? hm.get('B')/3 : 0;
+        int silver =hm.containsKey('S') ?  (hm.get('S')/3) + bronze : 0;
+        int gold = hm.get('G') + silver;
+        return gold;
     }
 }
