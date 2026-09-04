e1d532be9914c36f4d86df2af2c7783e6592695d
ELLA LAKSHMAN <laxmannani960@gmail.com>
2026-09-04 18:45:41 +0530

    Time: 7 ms (4.47%) | Memory: 46.8 MB (15.74%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -1,23 +1,18 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         if(nums.length <= 1) return 0;
-        int[] maxPre = new int[nums.length];
         int[] minSuff = new int[nums.length];
         int n = nums.length;
-        maxPre[0] = nums[0];
-        for(int i=1;i<nums.length;i++){
-            maxPre[i] = Math.max(nums[i],maxPre[i-1]);
-        }
-        
         minSuff[n-1]=nums[n-1];
         for(int j=n-2;j>=0;j--){
             minSuff[j] = Math.min(nums[j],minSuff[j+1]);
         }
-        System.out.println(Arrays.toString(maxPre));
         System.out.println(Arrays.toString(minSuff));
         int bestK = -1;
+        int maxPre = 0;
         for(int m=0;m<nums.length;m++){
-            int instability  = maxPre[m] - minSuff[m];
+            maxPre = Math.max(nums[m],maxPre);
+            int instability  = maxPre - minSuff[m];
             if(instability  <= k){
                 return m;
             }
