8e3abbe725e37ec511f9fa98af42e725c8a5246e
ELLA LAKSHMAN <laxmannani960@gmail.com>
2026-09-18 19:33:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (41.29%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -2,20 +2,18 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1)return nums[0];
-        int[] temp1 = Arrays.copyOfRange(nums,0,n-1);
-        int[] temp2 = Arrays.copyOfRange(nums,1,n);
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        int first = solve(temp1,0,dp);
+        int first = solve(nums,0,n-1,dp);
         Arrays.fill(dp,-1);
-        int second = solve(temp2,0,dp);
+        int second = solve(nums,1,n,dp);
         return Math.max(first,second);
     }
-    public int solve(int[] nums,int i,int[] dp){
-        if(i >= nums.length) return 0;
+    public int solve(int[] nums,int i,int end,int[] dp){
+        if(i >= end) return 0;
         if(dp[i] != -1)return dp[i];
-        int pick = nums[i] + solve(nums,i+2,dp);
-        int skip = solve(nums,i+1,dp);
+        int pick = nums[i] + solve(nums,i+2,end,dp);
+        int skip = solve(nums,i+1,end,dp);
         return dp[i] = Math.max(pick,skip);
     }
 }
