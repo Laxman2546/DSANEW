061c04020c8d06dd1a89b489ff41c3229a3ece87
ELLA LAKSHMAN <laxmannani960@gmail.com>
2026-09-24 19:55:32 +0530

    Time: 5 ms (13.35%) | Memory: 44.1 MB (5.75%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,7 +2,9 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> sk = new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(!sk.isEmpty()&& sk.peek()=='(' && s.charAt(i)== ')' ||
+            if(!sk.isEmpty()&& s.charAt(i) == '(' ||s.charAt(i) == '['||s.charAt(i) == '{'){
+                sk.push(s.charAt(i));
+            }else if(!sk.isEmpty()&& sk.peek()=='(' && s.charAt(i)== ')' ||
             !sk.isEmpty()&&
                 sk.peek()=='[' && s.charAt(i)== ']'||!sk.isEmpty()&&sk.peek()=='{' && s.charAt(i)== '}'){
                 sk.pop();
